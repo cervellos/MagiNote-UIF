a8a64b6c1a2742e5f39b273b55cd85ee76bc2447
Freddy Guerra <arcobaleno262@gmail.com>
2023-10-11 02:21:21 -0300

    esquema type

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,41 @@
+import { useState } from "react";
 import "./App.css";
 
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+};
+
 const App = () => {
+  const [notes, setNotes] = useState<Note[]>([
+    {
+      id: 1,
+      title: "note title 1",
+      content: "content 1",
+    },
+    {
+      id: 2,
+      title: "note title 2",
+      content: "content 2",
+    },
+    {
+      id: 3,
+      title: "note title 3",
+      content: "content 3",
+    },
+    {
+      id: 4,
+      title: "note title 4",
+      content: "content 4",
+    },
+    {
+      id: 5,
+      title: "note title 5",
+      content: "content 5",
+    },
+  ]);
+
   return (
     <div className="app-container">
       <form className="note-form">
@@ -9,6 +44,15 @@ const App = () => {
         <button type="submit">Add Note</button>
       </form>
       <div className="notes-grid">
+        {notes.map((notes) => (
+          <div className="notes-item">
+            <div className="notes-header">
+              <button>x</button>
+            </div>
+            <h2>{notes.title}</h2>
+            <p>{notes.content}</p>
+          </div>
+        ))}
         <div className="notes-item">
           <div className="notes-header">
             <button>x</button>
