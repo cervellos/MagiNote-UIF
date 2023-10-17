606432d76eac1f1e62c7a0175e73f82128d0e8da
Freddy Guerra <arcobaleno262@gmail.com>
2023-10-17 20:22:45 -0300

    created note

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ type Note = {
 };
 
 const App = () => {
+  //Note is a exportation in Typescript, allow to vinculated "notes" with the "Note"
   const [notes, setNotes] = useState<Note[]>([
     {
       id: 1,
@@ -35,17 +36,64 @@ const App = () => {
       content: "content 5",
     },
   ]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const [selectedNote, setSelectedNote] = useState<Note | null>(null);
+
+  //add new note with click and set el value
+  const handleNoteClick = (note: Note) => {
+    setSelectedNote(note);
+    setTitle(note.title);
+    setContent(note.content);
+  };
+
+  const hanbleAddNote = (
+    //take event as a paramert
+    event: React.FormEvent
+  ) => {
+    event.preventDefault();
+
+    const newNote: Note = {
+      id: notes.length + 1,
+      title: title,
+      content: content,
+    };
+    setNotes([newNote, ...notes]);
+    setTitle("");
+    setContent("");
+  };
 
   return (
     <div className="app-container">
-      <form className="note-form">
-        <input placeholder="title" required></input>
-        <textarea placeholder="Content" rows={10} required></textarea>
+      <form
+        className="note-form"
+        onSubmit={(event) => {
+          hanbleAddNote(event);
+        }}
+      >
+        <input
+          value={title}
+          //change when enter and set value Note
+          onChange={(event) => setTitle(event.target.value)}
+          placeholder="title"
+          required
+        ></input>
+        <textarea
+          value={content}
+          //not change with enter just set value in Note
+          onChange={(event) => {
+            setContent(event.target.value);
+          }}
+          placeholder="Content"
+          rows={10}
+          required
+        ></textarea>
         <button type="submit">Add Note</button>
       </form>
       <div className="notes-grid">
         {notes.map((notes) => (
-          <div className="notes-item">
+          <div className="notes-item" onClick={() => handleNoteClick(notes)}>
             <div className="notes-header">
               <button>x</button>
             </div>
